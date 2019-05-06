0b86c3d4ffdeb9ef5bfe780de54bffeaa56ae178
Jason M Harrison <harrisonj@dnb.com>
2019-05-06 14:41:55 -0500

    Final tests for Search Message component. Snapshots for tests.

--- src/components/search/SearchMessage.test.js
+++ src/components/search/SearchMessage.test.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SearchMessage from './SearchMessage';
+import React from 'react'
+import { shallow } from 'enzyme'
+import SearchMessage from './SearchMessage'
 
 const compName = 'SearchMessage'
+const exampleMessageBody = {
+	headline: 'This Headline',
+	message: 'This Message'
+}
 
 describe(`${compName} =====================`, () => {
 
   it(`renders without crashing`, () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<SearchMessage />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    shallow(<SearchMessage />)
+  });
+
+  it('renders correctly with no props', () => {
+  	const component = shallow(<SearchMessage />);
+	expect(component).toMatchSnapshot();
+  });
+
+  it('displays passed headline and message', () => {
+    const searchMessage = shallow(<SearchMessage headline={ exampleMessageBody.headline } message={ exampleMessageBody.message } />)
+    expect(searchMessage.find('h2').text()).toEqual(exampleMessageBody.headline)
+  	expect(searchMessage.find('p').text()).toEqual(exampleMessageBody.message)
   });
 
 });
