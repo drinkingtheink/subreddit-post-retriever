9b3d11c93db5f03de26d5bee7ced37d4222b40f5
Jason M Harrison <harrisonj@dnb.com>
2019-05-06 14:49:43 -0500

    RedditLogo tests.

--- src/components/search/RedditLogo.test.js
+++ src/components/search/RedditLogo.test.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import RedditLogo from './RedditLogo';
+import React from 'react'
+import { shallow } from 'enzyme'
+import RedditLogo from './RedditLogo'
 
 const compName = 'RedditLogo'
 
 describe(`${compName} =====================`, () => {
 
   it(`renders without crashing`, () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<RedditLogo />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    shallow(<RedditLogo />)
   });
 
+  it('renders correctly with no props', () => {
+  	const component = shallow(<RedditLogo />);
+	expect(component).toMatchSnapshot();
+  });
+
+  it('displays active state if fetchingData = true', () => {
+    const redditLogo = shallow(<RedditLogo fetchingData={ true } resultsDisplayed={ true } />)
+    expect(redditLogo.hasClass('fetching')).toEqual(true)
+  });
 });
